61450f83db8a4a865a86042a2c02791bc7aa1965
Kamil Paluch <kamil.paluch98@gmail.com>
2020-05-12 13:57:12 +0200

    add: Multiclients, Singleton, ThreadPool and Tasking

---
+++ Projekt_ps/SingletonSecured.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_ps
+{
+    public sealed class SingletonSecured
+    {
+        private static SingletonSecured m_oInstance = null;
+        private static readonly object m_oPadLock = new object();
+        private int m_nCounter = 0;
+
+        public static SingletonSecured Instance
+        {
+            get
+            {
+                lock (m_oPadLock)
+                {
+                    if (m_oInstance == null)
+                    {
+                        m_oInstance = new SingletonSecured();
+                    }
+                    return m_oInstance;
+                }
+            }
+        }
+
+        public Task AddTask(Task x)
+        {
+            return x;
+        }
+
+        private SingletonSecured()
+        {
+            m_nCounter = 1;
+        }
+
+    }
+}

fba332378721fb4adc2b674f3382f39cfec6c466
Kamil Paluch <kamil.paluch98@gmail.com>
2020-05-12 13:57:40 +0200

    add: Multiclients, Singleton, ThreadPool and Tasking

--- Projekt_ps/MainWindow.xaml.cs
+++ Projekt_ps/MainWindow.xaml.cs
@@ -10,6 +10,8 @@ using MySql.Data.MySqlClient;
 using System.Threading;
 using System.Data;
 using System.ComponentModel;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Projekt_ps
 {
@@ -17,51 +19,149 @@ namespace Projekt_ps
     {
         private MySqlConnection msqlcon = new MySqlConnection("Server=remotemysql.com;Port=3306;Database=DTzose51Js;Uid=DTzose51Js;pwd=ER2VaqmwbB;");
         private int users_counter=0;
+        private static int active_users=0;
+        private static readonly Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        private static readonly List<Socket> clientSockets = new List<Socket>();
+        private const int BUFFER_SIZE = 2048;
+        private const int PORT = 8888;
+        private static readonly byte[] buffer = new byte[BUFFER_SIZE];
+        private BackgroundWorker m_oBackgroundWorker = null;
+        private static List<Task> tasklist = new List<Task>();
 
         public MainWindow()
         {
+            ThreadPool.SetMaxThreads(10, 20);
             InitializeComponent();
-            DatabaseConnection();
+            DatabaseConnection();           
+            Main();
         }
 
-        /*public void DatabaseConnection_test()
+        private void Main()
         {
+            int port = 8888;
 
-            MySqlConnection msqlcon = new MySqlConnection("Server=remotemysql.com;Port=3306;Database=DTzose51Js;Uid=DTzose51Js;pwd=ER2VaqmwbB;");
-            MySqlDataReader read;
-            DataTable dt = new DataTable();
-            string ask = "SELECT * FROM Test_table";
+            if (null == m_oBackgroundWorker)
+                {
+                    m_oBackgroundWorker = new BackgroundWorker();
+                    m_oBackgroundWorker.DoWork += new DoWorkEventHandler(m_oBackgroundWorker_DoWork);
+                }
+                m_oBackgroundWorker.RunWorkerAsync(port);
+        }
+
+        private void m_oBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Dispatcher.Invoke(() => { lst_spis.Items.Add("Setting up server..."); });
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AcceptCallback, null);
+            Dispatcher.Invoke(() => { lst_spis.Items.Add("Server setup complete"); });
+            while (true)
+            {
+                Task.WaitAll(tasklist.ToArray());
+            }
+        }
+
+        private void AcceptCallback(IAsyncResult AR)
+        {
+            Socket socket;
 
             try
             {
-                msqlcon.Open();
-                MessageBox.Show("Połączono z bazą danych", "Sukces");
+                socket = serverSocket.EndAccept(AR);
+            }
+            catch (ObjectDisposedException) // I cannot seem to avoid this (on exit when properly closing sockets)
+            {
+                return;
+            }
 
-                MySqlCommand task = new MySqlCommand(ask, msqlcon);
-                read = task.ExecuteReader();
-                dt.Load(read);
-                DataRow dr;
-                lst_test.Items.Add("[ID] [login]");
+            clientSockets.Add(socket);
+            socket.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, socket);
+            Dispatcher.Invoke(() => { lst_spis.Items.Add("Client "+ socket.RemoteEndPoint +" connected, waiting for request..."); });
+            updateCounterOfActiveUsers(true);
+            serverSocket.BeginAccept(AcceptCallback, null);
+        }
 
-                for(int i = 0; i < dt.Rows.Count; i++)
+        private void ReceiveCallback(IAsyncResult AR)
+        {
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                Dispatcher.Invoke(() => { lst_spis.Items.Add("Client: " + current.RemoteEndPoint + " forcefully disconnected"); });
+                updateCounterOfActiveUsers(false);
+                // Don't shutdown because the socket may be disposed and its disconnected anyway.
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] recBuf = new byte[received];
+            Array.Copy(buffer, recBuf, received);
+            string text = Encoding.ASCII.GetString(recBuf);
+
+            if (text.ToLower() == "exit") // Client wants to exit gracefully
+            {
+                Dispatcher.Invoke(() => { lst_spis.Items.Add("Client: " + current.RemoteEndPoint + " disconnected"); }); 
+                updateCounterOfActiveUsers(false);
+                // Always Shutdown before closing
+                current.Shutdown(SocketShutdown.Both);
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+            else
+            {
+                string[] roger = null;
+                try
                 {
-                    dr = dt.Rows[i];
-                    string data = " ";
-                    for(int j = 0; j< dr.ItemArray.Count(); j++)
-                    {
-                        data += dr[j].ToString() + "    ";
-                    }
-                    lst_test.Items.Add(data);
+                    roger = text.Split(';');
+                }
+                catch
+                {
+                    Dispatcher.Invoke(() => { lst_spis.Items.Add("Client: " + current.RemoteEndPoint + " used unknown commend"); });
                 }
 
-                read.Close();
-                msqlcon.Close();
+                if (roger[0].ToLower() == "registration")
+                {
+                    /*try
+                    {*/
+                        Task t = new Task(() => { Dispatcher.Invoke(() => { addUser(roger[1], roger[2]); }); });
+                        tasklist.Add(SingletonSecured.Instance.AddTask(t));
+                        t.Start();
+                    //}
+                    /*catch(Exception e)
+                    {
+                        MessageBox.Show(e.ToString());
+                    }*/
+                }
+                //Console.WriteLine("Text is an invalid request");
+                //byte[] data = Encoding.ASCII.GetBytes("Invalid request");
+                //current.Send(data);
+                //Console.WriteLine("Warning Sent");
+                //Task t = new Task(() => { Dispatcher.Invoke(() => { lst_spis.Items.Add("Received Text from " + current.RemoteEndPoint + ": " + text); }); });
+                //tasklist.Add(SingletonSecured.Instance.AddTask(t));
+                //t.Start();
             }
-            catch
+
+            current.BeginReceive(buffer, 0, BUFFER_SIZE, SocketFlags.None, ReceiveCallback, current);
+        }
+
+        private void updateCounterOfActiveUsers(bool x)
+        {
+            if (x)
             {
-                MessageBox.Show("Nie udało się połączyć z bazą danych", "Błąd");
+                Dispatcher.Invoke(() => { lbl_active_users.Content = ++active_users; });               
             }
-        }*/
+            else
+            {
+                Dispatcher.Invoke(() => { lbl_active_users.Content = --active_users; });
+            }
+        }
 
         private void DatabaseConnection()
         {
@@ -79,19 +179,27 @@ namespace Projekt_ps
             }
         }
 
-        private void addUser()
+        private void addUser(string l,string p)
         {
             try
             {
-                string ask = "INSERT INTO Users(ID,Login,Password) VALUES (" + (users_counter + 1) +  ",'kot','pies')";
+                string ask = "INSERT INTO Users(ID,Login,Password) VALUES (" + (users_counter+1) +  ",'" + l + "','" + p+"')";
                 MySqlCommand task = new MySqlCommand(ask, msqlcon);
                 task.ExecuteNonQuery();
-                users_counter++;
                 lbl_all_users.Content = users_counter.ToString();
             }
-            catch
+            catch(MySqlException)
             {
-                MessageBox.Show("Taki użytkownik już istnieje!", "Błędny login!");
+                MessageBox.Show("Change your account login!", "Duplicated login!");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+            }
+            finally
+            {
+                users_counter = CheckUsers();
+                lbl_all_users.Content = users_counter.ToString();
             }
         }
 
@@ -111,7 +219,7 @@ namespace Projekt_ps
             }
             catch
             {
-                MessageBox.Show("Nie udało się połączyć z bazą danych", "Błąd");
+                MessageBox.Show("Failed to connect to the database!", "Connection error");
                 return 0;
             }
         }    
@@ -125,6 +233,45 @@ namespace Projekt_ps
                 msqlcon.Close();
             }
         }
+
+        /*public void DatabaseConnection_test()
+{
+
+    MySqlConnection msqlcon = new MySqlConnection("Server=remotemysql.com;Port=3306;Database=DTzose51Js;Uid=DTzose51Js;pwd=ER2VaqmwbB;");
+    MySqlDataReader read;
+    DataTable dt = new DataTable();
+    string ask = "SELECT * FROM Test_table";
+
+    try
+    {
+        msqlcon.Open();
+        MessageBox.Show("Połączono z bazą danych", "Sukces");
+
+        MySqlCommand task = new MySqlCommand(ask, msqlcon);
+        read = task.ExecuteReader();
+        dt.Load(read);
+        DataRow dr;
+        lst_test.Items.Add("[ID] [login]");
+
+        for(int i = 0; i < dt.Rows.Count; i++)
+        {
+            dr = dt.Rows[i];
+            string data = " ";
+            for(int j = 0; j< dr.ItemArray.Count(); j++)
+            {
+                data += dr[j].ToString() + "    ";
+            }
+            lst_test.Items.Add(data);
+        }
+
+        read.Close();
+        msqlcon.Close();
+    }
+    catch
+    {
+        MessageBox.Show("Nie udało się połączyć z bazą danych", "Błąd");
+    }
+}*/
 
     }
 }
